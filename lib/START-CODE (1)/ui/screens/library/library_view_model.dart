4ac9649f7630c44bbc8a124857475fab98dd042c
Ann-Arch-gif <an.heak123@student.cadt.edu.kh>
2026-03-04 03:47:52 +0700

    4. Implement LibraryViewModel - init, listen to PlayerState, expose getters and actions

--- lib/START-CODE (1)/ui/screens/library/library_view_model.dart
+++ lib/START-CODE (1)/ui/screens/library/library_view_model.dart
@@ -1,17 +1,63 @@
 import 'package:flutter/widgets.dart';
 
 import '../../../data/repositories/songs/song_repository.dart';
+import '../../../model/songs/song.dart';
 import '../../states/player_state.dart';
 
 /// ViewModel for the Library screen.
-/// Will be fully implemented in Part 4.
+/// Orchestrates data from SongRepository and PlayerState.
 class LibraryViewModel extends ChangeNotifier {
   final SongRepository songRepository;
   final PlayerState playerState;
 
+  List<Song> _songs = [];
+
   LibraryViewModel({
     required this.songRepository,
     required this.playerState,
-  });
-  // TODO: Full implementation in Part 4
+  }) {
+    init();
+    // Listen to PlayerState changes and notify this ViewModel's listeners
+    playerState.addListener(_onPlayerStateChanged);
+  }
+
+  /// Fetch songs from the repository
+  void init() {
+    _songs = songRepository.fetchSongs();
+    notifyListeners();
+  }
+
+  /// Called when PlayerState changes
+  void _onPlayerStateChanged() {
+    notifyListeners();
+  }
+
+  // --- Getters: expose all UI data ---
+
+  /// List of all songs
+  List<Song> get songs => _songs;
+
+  /// The currently playing song (if any)
+  Song? get currentSong => playerState.currentSong;
+
+  /// Check if a specific song is currently playing
+  bool isPlaying(Song song) => playerState.currentSong == song;
+
+  // --- User actions ---
+
+  /// Start playing a song
+  void play(Song song) {
+    playerState.start(song);
+  }
+
+  /// Stop the current song
+  void stop() {
+    playerState.stop();
+  }
+
+  @override
+  void dispose() {
+    playerState.removeListener(_onPlayerStateChanged);
+    super.dispose();
+  }
 }
