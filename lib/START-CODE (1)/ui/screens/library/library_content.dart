c0896c553910241decc265d98ce97ac0f3f25b35
Ann-Arch-gif <an.heak123@student.cadt.edu.kh>
2026-03-04 03:09:07 +0700

    1. Create two new classes (LibraryContent, LibraryViewModel)

---
+++ lib/START-CODE (1)/ui/screens/library/library_content.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// Content widget for the Library screen.
+/// Will receive its data from LibraryViewModel.
+/// Will be implemented in Part 2 & 5.
+class LibraryContent extends StatelessWidget {
+  const LibraryContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Implement in Part 2
+    return const Placeholder();
+  }
+}
---
+++ lib/START-CODE (1)/ui/screens/library/library_view_model.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+/// ViewModel for the Library screen.
+/// Will be implemented in Part 4.
+class LibraryViewModel extends ChangeNotifier {
+  // TODO: Implement in Part 4
+}
